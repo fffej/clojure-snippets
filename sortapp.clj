b54b96ec6b8d5226e163af3dfee8c9384b1f18e5
Jeff Foster <jeff.foster@acm.org>
2008-12-29 07:04:46 +0000

    Work out how to get the width and height of the canvas (this isn't implicit)

--- sortapp.clj
+++ sortapp.clj
@@ -1,6 +1,6 @@
 (import '(javax.swing JFrame JLabel JTextField JButton JComboBox JPanel Timer)
         '(java.awt.event ActionListener)
-        '(java.awt GridLayout))
+        '(java.awt Color GridLayout))
 
 (defn draw-sort [canvas alg]
   (prn alg))
@@ -8,7 +8,9 @@
 (def canvas (proxy [JPanel] []
   (paintComponent [g]
     (proxy-super paintComponent g)
-    (prn "Paint method called"))))
+    (.setColor g Color/RED)
+    (let [x (.getWidth this) y (.getHeight this)]
+      (prn x y)))))
 
 (defn sortapp []
   (let [frame (JFrame. "Sort Visualizer")
