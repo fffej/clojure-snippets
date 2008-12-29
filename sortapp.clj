808753cc640475a9b3be3e248f0b0134a03d1bd1
Jeff Foster <jeff.foster@acm.org>
2008-12-29 06:45:47 +0000

    Add the sort application

---
+++ sortapp.clj
@@ -0,0 +1,31 @@
+(import '(javax.swing JFrame JLabel JTextField JButton JComboBox JPanel Timer)
+        '(java.awt.event ActionListener)
+        '(java.awt GridLayout))
+
+(defn draw-sort [canvas alg]
+  (prn alg))
+
+(def canvas (proxy [JPanel] []
+  (paintComponent [g]
+    (proxy-super paintComponent g)
+    (prn "Paint method called"))))
+
+(defn sortapp []
+  (let [frame (JFrame. "Sort Visualizer")
+	algorithm-chooser (JComboBox.)
+	run-button (JButton. "Run Algorithm")]
+    (.addActionListener run-button
+      (proxy [ActionListener] []
+	(actionPerformed [evt] 
+	 (draw-sort canvas (.getSelectedItem algorithm-chooser)))))
+    (doto algorithm-chooser
+      (.addItem "Quick sort")
+      (.addItem "Bubble sort"))
+    (doto frame
+      (.setLayout (GridLayout. 2 2 3 3))
+      (.add algorithm-chooser)
+      (.add canvas)
+      (.add run-button)
+      (.setSize 300 300)
+      (.setVisible true))))
+	
