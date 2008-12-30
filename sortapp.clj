21ecb80d1f11108e693ef9e2633f7c1c3a770cbe
Jeff Foster <jeff.foster@acm.org>
2008-12-30 20:49:43 +0000

    An actual working version

--- sortapp.clj
+++ sortapp.clj
@@ -2,9 +2,35 @@
         '(java.awt.event ActionListener)
         '(java.awt Color GridLayout))
 
+;; Not so good quick sort
+(defn qsort [lst]
+  (if (nil? lst)
+    lst
+    (concat
+     (qsort (filter (partial > (first lst)) (rest lst)))
+     (list (first lst))
+     (qsort (filter (partial <= (first lst)) (rest lst))))))
+
+
+(defn bubble [lst]
+  (if (or (nil? lst) (nil? (second lst)))
+    lst
+    (if (> (first lst) (second lst))
+      (lazy-cons (second lst) (lazy-cons (first lst) (bubble (nthrest lst 2))))
+      (lazy-cons (first lst) (bubble (rest lst))))))
+
+;; Bubble sort
+(defn bubble-sort [lst]
+   (take (count lst) (iterate bubble lst)))
+
+
 (def maxval 100)
 
 (def model (take 100 (repeatedly (fn [] (rand-int maxval)))))
+
+(def bubble-sorted (bubble-sort model))
+
+(def position (atom 0))
 
 (def canvas (proxy [JPanel ActionListener] []
   (paintComponent [g]
@@ -12,12 +38,12 @@
     (.setColor g Color/RED)
     (let [width (.getWidth this) height (.getHeight this) barHeight (/ height (inc (count model))) barWidthPerVal (/ width maxval)]
       (prn width height)
-      (doseq [val (into (sorted-map) (zipmap (range 0 (count model)) model))] 
+      (doseq [val (into (sorted-map) (zipmap (range 0 (count model)) (nth bubble-sorted @position)))] 
 	(let [y (int (* (first val) barHeight)) barWidth (int (* (second val) barWidthPerVal))]
 	  (.fillRect g 0 y barWidth barHeight)))))
-  (actionPerformed [e] (prn "Doing something"))))
+  (actionPerformed [e] (swap! position inc) (.repaint this))))
 
-(let [x (Timer. 1000 canvas)]
+(let [x (Timer. 250 canvas)]
   (defn stop-timer [] (.stop x))
   (defn start-timer [] (.start x))
   (defn is-running [] (.isRunning x)))
