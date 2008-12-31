acf74b8be1b10e345f92c4551010b7d524cb2eb6
Jeff Foster <jeff.foster@acm.org>
2008-12-31 15:37:14 +0000

    Let's face it, I can't be bothered with writing the quicksort one, so get rid of that to tidy up the UI.

--- sortapp.clj
+++ sortapp.clj
@@ -1,16 +1,6 @@
-(import '(javax.swing JFrame JLabel JTextField JButton JComboBox JPanel Timer)
+(import '(javax.swing JFrame JButton JPanel Timer)
         '(java.awt.event ActionListener)
         '(java.awt Color BorderLayout))
-
-;; Not so good quick sort
-(defn qsort [lst]
-  (if (nil? lst)
-    lst
-    (concat
-     (qsort (filter (partial > (first lst)) (rest lst)))
-     (list (first lst))
-     (qsort (filter (partial <= (first lst)) (rest lst))))))
-
 
 (defn bubble [lst]
   (if (or (nil? lst) (nil? (second lst)))
@@ -19,15 +9,12 @@
       (lazy-cons (second lst) (lazy-cons (first lst) (bubble (nthrest lst 2))))
       (lazy-cons (first lst) (bubble (rest lst))))))
 
-;; Bubble sort
 (defn bubble-sort [lst]
    (take (count lst) (iterate bubble lst)))
 
 (def maxval 100)
 
-(def model (take 100 (repeatedly (fn [] (rand-int maxval)))))
-
-(def bubble-sorted (bubble-sort model))
+(def initial-list (take 100 (repeatedly (fn [] (rand-int maxval)))))
 
 (def position (atom 0))
 
@@ -35,39 +22,31 @@
   (paintComponent [g]
     (proxy-super paintComponent g)
     (.setColor g Color/RED)
-    (let [width (.getWidth this) height (.getHeight this) barHeight (/ height (inc (count model))) barWidthPerVal (/ width maxval)]
-      (if (< @position (count model))
-      (doseq [val (into (sorted-map) (zipmap (range 0 (count model)) (nth bubble-sorted @position)))] 
+    (let [width (.getWidth this) height (.getHeight this) barHeight (/ height (inc (count initial-list))) barWidthPerVal (/ width maxval)
+	  bubble-sorted (bubble-sort initial-list)]
+      (doseq [val (into (sorted-map) (zipmap (range 0 (count initial-list)) (nth bubble-sorted @position)))] 
 	(let [y (int (* (first val) barHeight)) barWidth (int (* (second val) barWidthPerVal))]
-	  (.fillRect g 0 y barWidth barHeight))))))
-  (actionPerformed [e] (swap! position inc) (compare-and-set! position (count model) 0) (.repaint this))))
+	  (.fillRect g 0 y barWidth barHeight)))))
+  (actionPerformed [e] (swap! position inc) (compare-and-set! position (count initial-list) 0) (.repaint this))))
 
-(let [x (Timer. 250 canvas)]
+(let [x (Timer. 50 canvas)]
   (defn stop-timer [] (.stop x))
   (defn start-timer [] (.start x))
   (defn is-running [] (.isRunning x)))
 
-(defn draw-sort [canvas alg]
-  (if (is-running)
-    (stop-timer)
-    (start-timer)))
-
 (defn sortapp []
   (let [frame (JFrame. "Sort Visualizer")
 	algorithm-chooser (JComboBox.)
-	run-button (JButton. "Run Algorithm")]
+	run-button (JButton. "Go")]
     (.addActionListener run-button
       (proxy [ActionListener] []
-	(actionPerformed [evt] 
-	 (draw-sort canvas (.getSelectedItem algorithm-chooser)))))
-    (doto algorithm-chooser
-      (.addItem "Quick sort")
-      (.addItem "Bubble sort"))
-    (doto frame
+	(actionPerformed [evt]
+          (if (is-running) 
+	    (stop-timer)
+	    (start-timer)))))
+     (doto frame
       (.setLayout (BorderLayout. 3 3))
-      (.add algorithm-chooser (BorderLayout/PAGE_START))
       (.add canvas (BorderLayout/CENTER))
       (.add run-button (BorderLayout/PAGE_END))
       (.setSize 300 300)
       (.setVisible true))))
-	
