97c73b272df1976794f88cf156390ae89598a81d
Jeff Foster <jeff.foster@acm.org>
2009-01-05 11:54:42 +0000

    Shove in some drawing code

--- gameoflife.clj
+++ gameoflife.clj
@@ -1,13 +1,19 @@
 ;;; Implementation of the Game of Life
 ;;; jeff.foster@acm.org
 
+(import '(javax.swing JFrame JLabel JTextField JButton JComboBox JPanel Timer)
+       '(java.awt.event ActionListener)
+       '(java.awt GridLayout Color))
+
+
+;; Rules from Wikipedia
 ;; 1. Any live cell with fewer than two live neighbours dies, as if by needs caused by underpopulation.
 ;; 2. Any live cell with more than three live neighbours dies, as if by overcrowding.
 ;; 3. Any live cell with two or three live neighbours lives, unchanged, to the next generation.
 ;; 4. Any tile with exactly three live neighbours cells will be populated with a living cell.
 
 (defn world-at [world x y]
-  (if (and (< x (count world)) (< y (count (first world))))
+  (if (and (>= x 0) (>= y 0) (< x (count world)) (< y (count (first world))))
     (nth (nth world x) y)
     0))
 
@@ -32,5 +38,30 @@
 (defn life-step [world]
   (let [width (count world) height (count (first world))]
     (map 
-     (fn [row] (map (fn [col] (second col)) (zipmap (range 0 height) (second row))))
+     (fn [row] (map (fn [col] 
+		      (let [x (first row) y (first col)]
+			(new-state world x y)))
+		    (zipmap (range 0 height) (second row))))
      (zipmap (range 0 width) world))))
+
+;; UI elements and mutable ness
+
+(def grid-size 16)
+
+(def world (atom (create-world grid-size grid-size)))
+
+(def canvas (proxy [JPanel] []
+  (paintComponent [g]
+    (proxy-super paintComponent g)
+    (doseq [x (range 0 grid-size)]
+      (doseq [y (range 0 grid-size)]
+	(let [alive (world-at @world x y)]
+	      (.fillRect g (* x grid-size) (* y grid-size) (dec grid-size) (dec grid-size))))))))
+
+(defn lifeapp []
+  (let [frame (JFrame. "Game of Life")]
+    (doto frame
+      (.add canvas)
+      (.setSize 300 300)
+      (.setResizable false)
+      (.setVisible true))))
