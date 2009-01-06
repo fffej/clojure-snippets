b53e96d2d462bc96e07ebf51fefeaad72abe3dd8
Jeff Foster <jeff.foster@acm.org>
2009-01-06 20:49:58 +0000

    Notify where the match actually is, but without highlighting it yet.

--- regex.clj
+++ regex.clj
@@ -7,12 +7,22 @@
        '(java.awt.event ActionListener KeyAdapter)
        '(java.awt GridLayout Color))
 
+;; TODO exeption handling
+(defn match [regex text]
+  (let [match (re-find (re-pattern regex) text)]
+    (if (nil? match)
+      [0 0]
+      (let [ind (.indexOf text match)]
+	[ind (+ ind (.length match))]))))
 
 (defn regexcoach []
   (let [frame (JFrame. "Regular Expression Coach") pane (JPanel.) regexText (JTextField.) 
 	targetText (JTextField.)
 	statusBar (JLabel. "Match from 0 to 0")
-        keyHandler (proxy [KeyAdapter] [] (keyTyped [keyEvent] (prn keyEvent))) ]
+        keyHandler (proxy [KeyAdapter] [] 
+		     (keyTyped [keyEvent] 
+		       (let [m (match (.getText regexText) (.getText targetText))]
+			 (.setText statusBar (format "Match from %s to %s" (first m) (second m))))))]
     (doto regexText
       (.addKeyListener keyHandler))
     (doto targetText
