c29d44e0d97237fc07cfd407e400ee56a956faf4
Jeff Foster <jeff.foster@acm.org>
2009-01-27 20:57:16 +0000

    Modify Eliza to support the segment-match function defined in 5.3 of PAIP.  Remove the less than amazing maze.clj!

--- eliza.clj
+++ eliza.clj
@@ -38,22 +38,76 @@
      (= input binding) bindings
      :else nil)))
 
-(defn pat-match
+(defn pat-match-v2
   "Match pattern against input in the context of the bindings"
   ([pattern input] 
-    (pat-match pattern input no-bindings))
+    (pat-match-v2 pattern input no-bindings))
   ([pattern input bindings]
     (cond
      (nil? bindings) nil
      (variable? pattern) (match-variable pattern input bindings)
      (= pattern input) bindings
      (and (seq? pattern) (seq? input))
-       (pat-match 
+       (pat-match-v2 
 	(rest pattern) 
 	(rest input)
-	(pat-match (first pattern) (first input) bindings))
+	(pat-match-v2 (first pattern) (first input) bindings))
      :else nil)))
 
+;; Segment pattern matching
+(defn segment-pattern? 
+  "Is this a segment matching pattern: ((?* var) . pattern)"
+  [pattern]
+  (and (seq? pattern) (seq? (first pattern)) (.startsWith (str (ffirst pattern)) "?*")))
+
+(defn position [elt vec start]
+  (if (>= start (count vec))
+    nil
+    (if (= (nth vec start) elt)
+      start
+      (recur elt vec (inc start)))))
+
+(declare pat-match)
+
+(defn sub-sequence 
+  ([lst start] 
+     (sub-sequence lst start (count lst)))
+  ([lst start end]
+  ((fn [lst x y accum]
+     (if (>= x y)
+       (reverse accum)
+       (recur (rest lst) (inc x) y (conj accum (first lst))))) (nthrest lst start) 0 (- end start) '())))
+
+(defn segment-match
+  "Match the segment pattern ((?* var) . pat) against input."
+  ([pattern input bindings]
+     (segment-match pattern input bindings 0))
+  ([pattern input bindings start]
+     (let [var (second (first pattern)) pat (rest pattern)]
+       (if (nil? pat)
+	   (match-variable var input bindings)
+	   (let [pos (position (first pat) input start)]
+	     (if pos
+	       (let [b2 (pat-match pat (sub-sequence input pos) (match-variable var (sub-sequence input 0 pos) bindings))]
+		 (if (nil? b2)
+		   (segment-match pattern input bindings (inc pos))
+		   b2))
+	       nil))))))
+
+(defn pat-match
+  "Match pattern against input in the context of the bindings"
+  ([pattern input]
+     (pat-match pattern input no-bindings))
+  ([pattern input bindings]
+     (cond
+      (nil? bindings) nil
+      (variable? pattern) (match-variable pattern input bindings)
+      (= pattern input) bindings
+      (segment-pattern? pattern) (segment-match pattern input bindings) ;;; To be determined
+      (and (seq? pattern) (seq? input))
+        (pat-match (rest pattern) (rest input) (pat-match (first pattern) (first input) bindings))
+      :else nil)))
+       
        
 
      
