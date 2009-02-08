254b7dc1b9f595ff8facb29fec230e11ab68574f
Jeff Foster <jeff.foster@acm.org>
2009-02-08 14:06:19 +0000

    Add support for padding

--- base64.clj
+++ base64.clj
@@ -12,6 +12,12 @@
 	c (bit-shift-right (bit-and num 258048) 12)
 	d (bit-shift-right (bit-and num 16515072) 18)]
     (map (fn [x] (nth *encode-table* x )) (list d b c a))))
+
+(defn str-pad [x size padchar]
+  (let [d (rem (count x) size)]
+    x
+    (apply str x (take (- size d) (repeat padchar)))))
+
 		   
 ; Need to implement padding
 (defn encode 
@@ -19,6 +25,6 @@
   [seq]
   (if (nil? seq)
     nil
-    (let [x (map int (take 3 seq))
+    (let [x (map int (str-pad (take 3 seq) 3 \=))
 	  num (+ (last x) (* 256 (second x)) (* 256 256 (first x)))]
       (lazy-cons (encode-num num) (encode (drop 3 seq))))))
